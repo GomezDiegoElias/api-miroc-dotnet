b24f871cebb9a45754d0cd69e03d7b0f461745e3
Trolsonjoel1992 <joelutn@gmail.com>
2025-10-16 08:23:46 -0300

    feat(construction): add entity

---
+++ src/org.apimiroc.core.entities/Entities/Construction.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace org.apimiroc.core.entities.Entities
+{
+    [Table("tbl_employee")]
+    public class Employee
+    {
+
+        [Key]
+        [Column("id")]
+        public string Id { get; set; }
+
+        [Required]
+        [Column("dni")]
+        public long Dni { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("workstation")]
+        public string WorkStation { get; set; }
+
+        [Required]
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; } = false;
+
+        [Required]
+        [Column("create_at")]
+        public DateTime CreateAt { get; set; } = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") != null ? DateTime.Now : DateTime.Now;
+
+        [Required]
+        [Column("update_at")]
+        public DateTime UpdateAt { get; set; } = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") != null ? DateTime.Now : DateTime.Now;
+
+        public Employee() { }
+
+        public Employee(string id, long dni, string firstName, string lastName, string workStation)
+        {
+            Id = id;
+            Dni = dni;
+            FirstName = firstName;
+            LastName = lastName;
+            WorkStation = workStation;
+        }
+
+        public static string GenerateId()
+        {
+            string timestamp = DateTimeOffset.UtcNow.ToString("yyyyMMddHHmmss");
+            string uuidPart = Guid.NewGuid().ToString().Split('-')[0];
+            return $"emp-{timestamp}-{uuidPart}";
+        }
+
+    }
+}
