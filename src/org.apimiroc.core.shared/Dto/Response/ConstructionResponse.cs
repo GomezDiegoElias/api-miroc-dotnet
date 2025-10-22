f750d1a9465a1a479181b0f90b770328af9f5463
Trolsonjoel1992 <joelutn@gmail.com>
2025-10-22 16:04:24 -0300

    feat(construction): add Response

---
+++ src/org.apimiroc.core.shared/Dto/Response/ConstructionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace org.apimiroc.core.shared.Dto.Response
+{
+    public record ConstrucctionResponse(
+        string Name,
+        DateTime StartDate,
+        DateTime EndDate,
+        string Address,
+        string Description
+    )
+    { }
+}
