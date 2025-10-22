89215807e94de36b862cb0e0f1caec6264b442e6
Trolsonjoel1992 <joelutn@gmail.com>
2025-10-22 16:06:10 -0300

    feat(construction): add Request

---
+++ src/org.apimiroc.core.shared/Dto/Request/ConstructionRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace org.apimiroc.core.shared.Dto.Request
+{
+    public record ConstructionRequest(
+        string Name,
+        DateTime StartDate,
+        DateTime EndDate,
+        string Address,
+        string Description
+    )
+    { }
+}
