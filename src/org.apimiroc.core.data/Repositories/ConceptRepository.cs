924e6630da6e605832f9324c80cb743e38ef259b
Diego <anikilogomez123@gmail.com>
2025-10-30 22:06:41 -0300

    feat(concept): agrega el repositorio y su implementacion con metodos crud basicos

--- src/org.apimiroc.core.data/Repositories/ConceptRepository.cs
+++ src/org.apimiroc.core.data/Repositories/ConceptRepository.cs
@@ -18,6 +18,18 @@ namespace org.apimiroc.core.data.Repositories
             _paginationRepository = paginationRepository;
         }
 
+        public async Task Delete(Concept concept)
+        {
+            concept.IsDeleted = true;
+            await _context.SaveChangesAsync();
+        }
+
+        //public async Task DeletePermanent(Concept concept)
+        //{
+        //    _context.Concepts.Remove(concept);
+        //    await _context.SaveChangesAsync();
+        //}
+
         public async Task<PaginatedResponse<Concept>> FindAll(ConceptFilter filters)
         {
 
@@ -43,11 +55,40 @@ namespace org.apimiroc.core.data.Repositories
 
         }
 
+        public async Task<Concept?> FindById(int id)
+        {
+            return await _context.Concepts.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Concept?> FindByName(string name)
+        {
+            return await _context.Concepts.FirstOrDefaultAsync(c => c.Name == name);
+        }
+
+        public async Task<Concept?> FindByType(string name)
+        {
+            return await _context.Concepts.FirstOrDefaultAsync(c => c.type == name);
+        }
+
         public async Task<Concept> Save(Concept concept)
         {
             _context.Concepts.Add(concept);
             await _context.SaveChangesAsync();
             return concept;
         }
+
+        public async Task<Concept> Update(Concept concept)
+        {
+            concept.Name = concept.Name;
+            concept.type = concept.type;
+            concept.Description = concept.Description;
+            await _context.SaveChangesAsync();
+            return concept;
+        }
+
+        //public Task<Concept> UpdatePartial(Concept concept)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
--- src/org.apimiroc.core.data/Repositories/Imp/IConceptRepository.cs
+++ src/org.apimiroc.core.data/Repositories/Imp/IConceptRepository.cs
@@ -8,5 +8,12 @@ namespace org.apimiroc.core.data.Repositories.Imp
     {
         public Task<PaginatedResponse<Concept>> FindAll(ConceptFilter filters);
         public Task<Concept> Save(Concept concept);
+        public Task<Concept?> FindById(int id);
+        public Task<Concept?> FindByName(string name);
+        public Task<Concept?> FindByType(string name);
+        public Task<Concept> Update(Concept concept);
+        //public Task<Concept> UpdatePartial(Concept concept);
+        public Task Delete(Concept concept);
+        //public Task DeletePermanent(Concept concept);
     }
 }
