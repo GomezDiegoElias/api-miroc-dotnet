669143a2cac78b7cbd32bfa5301f92cb96f893c2
Diego <anikilogomez123@gmail.com>
2025-10-30 22:07:37 -0300

    feat: agrega excepciones personalizadas para concepto y movimiento

---
+++ src/org.apimiroc.core.entities/Exceptions/MovementNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace org.apimiroc.core.entities.Exceptions
+{
+    public class MovementNotFoundException : Exception
+    {
+        public MovementNotFoundException(int id)
+            : base($"Movimiento con ID {id} no existe") { }
+    }
+}
