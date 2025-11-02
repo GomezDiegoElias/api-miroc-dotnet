ea2ba227144f3f944ce146dc159da04807a74f0e
Diego <anikilogomez123@gmail.com>
2025-11-02 20:50:40 -0300

    chore: agrega nuevas reglas en el .gitignore para archivos logs y txt

--- src/org.apimiroc.app/Program.cs
+++ src/org.apimiroc.app/Program.cs
@@ -2,8 +2,34 @@ using FluentValidation;
 using Newtonsoft.Json.Serialization;
 using org.apimiroc.app.Validations;
 using org.apimiroc.core.config;
+using Serilog;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de Serilog
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()       // Permite agregar propiedades de contexto
+        .Enrich.WithMachineName()      // Nombre de la máquina/servidor
+        .Enrich.WithThreadId()         // ID del hilo
+        .Enrich.WithExceptionDetails() // Excepciones enriquecidas
+        .WriteTo.Console(
+            outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] ({MachineName}/{ThreadId}) {Message:lj}{NewLine}{Exception}"
+        )
+        .WriteTo.File(
+            path: "../logs/log-.txt",             // Carpeta fuera del proyecto
+            rollingInterval: RollingInterval.Day, // Archivo nuevo cada día
+            retainedFileCountLimit: 7,            // Guarda solo 7 días
+            fileSizeLimitBytes: 10_000_000,       // 10 MB por archivo
+            rollOnFileSizeLimit: true,            // Crea nuevo si supera tamaño
+            shared: false,                        // Una sola instancia escribe
+            buffered: true                         // Escritura con buffer en memoria
+        );
+});
+
 
 // Configuración básica de servicios
 builder.Services.AddControllers()
@@ -46,8 +72,10 @@ DatabaseConfig.MigrateDatabase(app.Services);
 // Seeder
 //await DbSeederConfig.SeedAsync(app);
 
-app.MapGet("/", () =>
+app.MapGet("/", (ILogger<Program> logger) =>
 {
+
+    logger.LogInformation("API llamada correctamente al endpoint raíz '/'");
 
     var currentTime = DateTime.Now.ToString("HH:mm:ss");
 
